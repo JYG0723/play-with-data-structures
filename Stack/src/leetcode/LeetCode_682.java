70e91046a9186a7e239e32df00ff221a17c0861d
hikaricp <jiyongguang0723@gmail.com>
2018-04-19 15:34:44 +0800

    【majorization】

--- Stack/src/leetcode/LeetCode_682.java
+++ Stack/src/leetcode/LeetCode_682.java
@@ -11,31 +11,26 @@ public class LeetCode_682 {
     public int calPoints(String[] ops) {
         Stack<Integer> stack = new Stack<>();
         int sum = 0;
-        for (int i = 0; i < ops.length; i++) {
-            Object value = ops[i];
+        for (String value : ops) {
             if (value.equals("D")) {
                 int frontValue = ((int) stack.peek()) << 1;
-                sum += frontValue;
                 stack.push(frontValue);
-                continue;
-            }
-            if (value.equals("C")) {
-                int a = stack.pop();
-                sum -= a;
-                continue;
-            }
-            if (value.equals("+")) {
+            } else if (value.equals("C")) {
+                stack.pop();
+            } else if (value.equals("+")) {
                 int a = stack.pop();
                 int b = stack.peek();
                 int nowCount = a + b;
                 stack.push(a);
                 stack.push(nowCount);
-                sum += nowCount;
-                continue;
+            } else {
+                int element = Integer.valueOf(value);
+                stack.push(element);
             }
-            int element = Integer.valueOf((String) value);
-            stack.push(element);
-            sum += element;
+        }
+
+        for (int score : stack) {
+            sum += score;
         }
         return sum;
     }
