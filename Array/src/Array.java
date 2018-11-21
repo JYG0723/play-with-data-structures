4eff7e397a20d8e1806ce630c244abf8df7cfa5c
hikaricp <jiyongguang0723@gmail.com>
2018-11-21 11:01:30 +0800

    update Array structer

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -1,8 +1,11 @@
+import java.util.Objects;
+
 /**
  * @author Ji YongGuang.
  * @date 20:45 2018/4/16.
  * 自定义数组封装类
  */
+@SuppressWarnings("ALL")
 public class Array<E> {
 
     /**
@@ -52,31 +55,33 @@ public class Array<E> {
 
     /**
      * 所有元素后添加一个元素
+     * 由于底层调用add函数，有可能触发resize函数，所有按最坏情况考虑。
+     * 复杂度O(n)
      *
      * @param e
      */
     public void addLast(E e) {
-        /*if (size == data.length) {
-            throw new IllegalArgumentException("Array - addLast - data array has full");
-        }
-        data[size] = e;
-        size++;*/
         add(size, e);
     }
 
     /**
      * 在指定位置插入一个元素
+     * O(n)
      *
      * @param index
      * @param e
      */
     public void add(int index, E e) {
+
         if (index < 0 || index > size) {
             throw new IllegalArgumentException("Array - add - Required index > 0 && index < data.size");
         }
-        if (size == data.length) {
+
+        // 0.75
+        if (size >= data.length - (data.length >> 2)) {
             resize(data.length << 1);
         }
+
         for (int i = size - 1; i >= index; i--) {
             data[i + 1] = data[i];
         }
@@ -86,6 +91,7 @@ public class Array<E> {
 
     /**
      * 所有元素前插入一个元素
+     * O(n)
      *
      * @param e
      */
@@ -95,6 +101,7 @@ public class Array<E> {
 
     /**
      * 获取index索引位置的元素
+     * O(n)
      *
      * @param index
      * @return
@@ -122,6 +129,7 @@ public class Array<E> {
 
     /**
      * 查找数组中是否含有元素e
+     * O(n)
      *
      * @param e
      * @return
@@ -137,6 +145,7 @@ public class Array<E> {
 
     /**
      * 查找数组中元素e所在的索引，如果不存在返回-1
+     * O(n)
      *
      * @param e
      * @return
@@ -168,6 +177,11 @@ public class Array<E> {
         size--;
         data[size] = null;
 
+        /*
+         需要考虑容量不能为0 即 data.length != 1
+         我们不需要考虑 data.length / 2 == 0 时size是多少这么多，只要知道 size == data.length / 4 时会触发缩容
+            条件，而我们再给它加一层判断条件即可。即特殊情况不需要再触发缩容
+          */
         if (size == data.length / 4 && data.length / 2 != 0) {
             resize(data.length >> 1);
         }
@@ -176,6 +190,7 @@ public class Array<E> {
 
     /**
      * 从数组中删除头元素，并返回值
+     * O(n)
      *
      * @return
      */
@@ -185,6 +200,8 @@ public class Array<E> {
 
     /**
      * 从数组中删除尾元素，并返回值
+     * 由于底层调用remove函数，有可能触发resize函数，所有按最坏情况考虑。
+     * 复杂度O(n)
      *
      * @return
      */
@@ -201,6 +218,50 @@ public class Array<E> {
         int index = find(e);
         if (index != -1) {
             remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 统计元素数
+     *
+     * @param e
+     * @return
+     */
+    public int countElement(E e) {
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(data[i], e)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 删除整个容器中的元素e
+     *
+     * @param e
+     * @return
+     */
+    public boolean removeAllElement(E e) {
+        /*
+           错误逻辑
+           无法将index都记录到list中，因为每删一次元素，另一个匹配的e的index都将改变。
+        List<Integer> indexs = findAll(e);
+        for (int i = 0; i < indexs.size(); i++) {
+            remove(indexs.get(i));
+        }
+        return true;*/
+        int count = 0;
+        int countE = countElement(e);
+        while (find(e) != -1) {
+            count++;
+            removeElement(e);
+        }
+
+        if (count == countE) {
             return true;
         }
         return false;
@@ -223,13 +284,16 @@ public class Array<E> {
 
     /**
      * 将数组空间的容量变成newCapacity大小
+     * O(n)
      *
      * @param newCapacity
      */
     private void resize(int newCapacity) {
         E[] newData = (E[]) new Object[newCapacity];
-        for (int i = 0; i < size; i++)
+        /*for (int i = 0; i < data.length; i++) {
             newData[i] = data[i];
+        }*/
+        System.arraycopy(data, 0, newData, 0, data.length);
         data = newData;
     }
 }
--- Array/src/Main.java
+++ Array/src/Main.java
@@ -2,15 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Array<Integer> array = new Array<>(20);
+        Array<Integer> array = new Array<>();
         for (int i = 0; i < 10; i++) {
             array.addLast(i);
+            System.out.println(array);
         }
         System.out.println(array);
 
+        // 扩容
         array.add(1, 100);
         System.out.println(array);
 
+        array.addFirst(-1);
+        array.addFirst(-1);
         array.addFirst(-1);
         System.out.println(array);
 
@@ -22,5 +26,10 @@ public class Main {
 
         array.removeElement(100);
         System.out.println(array);
+
+
+        System.out.println(array.removeAllElement(-1));
+        System.out.println(array);
+
     }
 }
